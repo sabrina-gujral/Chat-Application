06e1277c0270836b5f8863b002080324219e10a1
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-06-02 18:07:27 +0530

    Implement chat application using socket.io

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { disconnect } = require("process");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+io.on("connection", (socket) => {
+  socket.on("username", (username) => {
+    socket.username = username;
+    io.emit("isOnline", "<i> " + socket.username + " joined the chat </i>");
+
+    socket.on("disconnect", (username) => {
+      io.emit("isOnline", "<i> " + socket.username + " left the chat </i>");
+    });
+
+    socket.on("chat", (message) => {
+      io.emit("chat", "<strong>" + socket.username + "</strong>" + message);
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000, () => console.log("server listening on port 3000..."));
